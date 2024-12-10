d101920c281a97234f495f4369aa795c5d4e8f21
Matthew Collins <matthew.collins@defra.gov.uk>
2024-12-10 12:44:49 +0000

    Remove some code duplication

---
+++ server/routes/getReservoirDryRisk.js
@@ -0,0 +1,53 @@
+function getReservoirWetRisk (riskQueryResult) {
+  let reservoirWetRisk
+  if (riskQueryResult.wetReservoirs?.length > 0) {
+    reservoirWetRisk = riskQueryResult.wetReservoirs.map(function (item) {
+      return {
+        reservoirName: item.attributes.RESERVOIR,
+        location: item.attributes.NGR,
+        riskDesignation: item.attributes.RISK_DESIGNATION,
+        undertaker: item.attributes.UNDERTAKER,
+        leadLocalFloodAuthority: item.attributes.LLFA_NAME,
+        comments: item.attributes.COMMENTS
+      }
+    })
+  } else {
+    reservoirWetRisk = riskQueryResult.wetReservoirs
+  }
+  return reservoirWetRisk
+}
+exports.getReservoirWetRisk = getReservoirWetRisk
+function getReservoirDryRisk (riskQueryResult) {
+  let reservoirDryRisk
+  if (riskQueryResult.dryReservoirs?.length > 0) {
+    reservoirDryRisk = riskQueryResult.dryReservoirs.map(function (item) {
+      return {
+        reservoirName: item.attributes.reservoir,
+        location: item.attributes.ngr,
+        riskDesignation: item.attributes.risk_designation,
+        undertaker: item.attributes.undertaker,
+        leadLocalFloodAuthority: item.attributes.llfa_name,
+        comments: item.attributes.comments
+      }
+    })
+  } else {
+    reservoirDryRisk = riskQueryResult.dryReservoirs
+  }
+  return reservoirDryRisk
+}
+exports.getReservoirDryRisk = getReservoirDryRisk
+
+const processAreaList = (areaList) => {
+  const areaListReturn = []
+  areaList?.forEach((area) => {
+    areaListReturn.push(area.attributes.FWS_TACODE)
+  })
+  return areaListReturn
+}
+exports.processAreaList = processAreaList
+
+const groundWaterAreaCheck = (areas) => {
+  return areas.find((area) => area.charAt(5) === 'G') // NOSONAR
+}
+
+exports.groundWaterAreaCheck = groundWaterAreaCheck
--- server/routes/reservoir.js
+++ server/routes/reservoir.js
@@ -1,6 +1,7 @@
 const joi = require('joi')
 const boom = require('@hapi/boom')
 const { reservoirQuery } = require('../services/riskQuery')
+const { getReservoirDryRisk, getReservoirWetRisk, processAreaList, groundWaterAreaCheck } = require('./getReservoirDryRisk')
 
 module.exports = {
   method: 'GET',
@@ -10,12 +11,12 @@ module.exports = {
     handler: async (request, _h) => {
       const params = request.params
 
-      const riskQueryResult = await reservoirQuery(params.x, params.y)
+      const reservoirQueryResult = await reservoirQuery(params.x, params.y)
 
       /*
         * Do some assertions around the result we get back from the database
         */
-      if (!riskQueryResult) {
+      if (!reservoirQueryResult) {
         return boom.badRequest('Invalid result', new Error('Invalid query response'))
       }
 
@@ -23,52 +24,15 @@ module.exports = {
         * If we get here we can be sure we have a valid result from
         * the ESRI database and we can start to prepare our return response
         */
-      let reservoirDryRisk = null
-      let reservoirWetRisk = null
+      const reservoirDryRisk = getReservoirDryRisk(reservoirQueryResult)
 
-      if (riskQueryResult.dryReservoirs?.length > 0) {
-        reservoirDryRisk = riskQueryResult.dryReservoirs.map(function (item) {
-          return {
-            reservoirName: item.attributes.reservoir,
-            location: item.attributes.ngr,
-            riskDesignation: item.attributes.risk_designation,
-            undertaker: item.attributes.undertaker,
-            leadLocalFloodAuthority: item.attributes.llfa_name,
-            comments: item.attributes.comments
-          }
-        })
-      } else {
-        reservoirDryRisk = riskQueryResult.dryReservoirs
-      }
-
-      if (riskQueryResult.wetReservoirs?.length > 0) {
-        reservoirWetRisk = riskQueryResult.wetReservoirs.map(function (item) {
-          return {
-            reservoirName: item.attributes.RESERVOIR,
-            location: item.attributes.NGR,
-            riskDesignation: item.attributes.RISK_DESIGNATION,
-            undertaker: item.attributes.UNDERTAKER,
-            leadLocalFloodAuthority: item.attributes.LLFA_NAME,
-            comments: item.attributes.COMMENTS
-          }
-        })
-      } else {
-        reservoirWetRisk = riskQueryResult.wetReservoirs
-      }
+      const reservoirWetRisk = getReservoirWetRisk(reservoirQueryResult)
 
       let isGroundwaterArea = false
-      const floodAlertAreas = []
-      riskQueryResult.floodAlertAreas?.forEach((area) => {
-        floodAlertAreas.push(area.attributes.FWS_TACODE)
-      })
-      const floodWarningAreas = []
-      riskQueryResult.floodWarningAreas?.forEach((area) => {
-        floodWarningAreas.push(area.attributes.FWS_TACODE)
-      })
+      const floodAlertAreas = processAreaList(reservoirQueryResult.floodAlertAreas)
+      const floodWarningAreas = processAreaList(reservoirQueryResult.floodWarningAreas)
 
-      const fifthChar = 5
-
-      if (floodAlertAreas.find((faa) => faa.charAt(fifthChar) === 'G') || floodWarningAreas.find((fwa) => fwa.charAt(fifthChar) === 'G')) {
+      if (groundWaterAreaCheck(floodAlertAreas) || groundWaterAreaCheck(floodWarningAreas)) {
         isGroundwaterArea = true
       }
 
